ea041be6b30e7048a8eb24ce4a205ad52aeb99d3
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2024-03-16 16:11:40 -0300

    feat: :zap: implement express-validator

---
+++ knex-project/middlewares/common.middlewares.js
@@ -0,0 +1,22 @@
+const { validationResult } = require("express-validator")
+const { badRequestResponse } = require("../utils/response.utils")
+
+const validate = validations => {
+  return async (req, res, next) => {
+    for (let validation of validations) {
+      const result = await validation.run(req)
+      if (result.errors.length) break
+    }
+
+    const errors = validationResult(req)
+    if (errors.isEmpty()) {
+      return next()
+    }
+
+    badRequestResponse(res, errors.array()[0].msg)
+  }
+}
+
+module.exports = {
+  validate
+}
