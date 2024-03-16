79ba48350d3f0359bfbae137a0b9a2ac4c044f51
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2024-03-15 22:39:20 -0300

    docs: :zap: improve and comment response utils

--- knex-project/utils/response.utils.js
+++ knex-project/utils/response.utils.js
@@ -1,52 +1,77 @@
 /**
- * @param {import('express').Response} response 
- * @param {any} data 
+ * Sends a successful response with data.
+ * 
+ * @param {import('express').Response} response The response object.
+ * @param {any} data The data to be sent in the response.
  */
 const okResponse = (response, data) => response.status(200).json({
   data,
   status: 200,
   errors: null
-})
+});
 
 /**
- * @param {import('express').Response} response 
+ * Sends a successful response with data and status 201 (Created).
+ * 
+ * @param {import('express').Response} response The response object.
+ * @param {any} data The data to be sent in the response.
  */
-const noContentResponse = (response) => response.status(204).end()
+const createdResponse = (response, data) => response.status(201).json({
+  data,
+  status: 201,
+  errors: null
+});
+
 
 /**
- * @param {import('express').Response} response 
- * @param {any} errors 
+ * Sends a no content response.
+ * 
+ * @param {import('express').Response} response The response object.
+ */
+const noContentResponse = (response) => response.status(204).end();
+
+
+/**
+ * Sends a bad request response with errors.
+ * 
+ * @param {import('express').Response} response The response object.
+ * @param {any} errors The errors to be sent in the response.
  */
 const badRequestResponse = (response, errors) => {
   let array = []
   if (Array.isArray(errors)) array = errors
   else array = [errors]
 
-  return response.status(500).json({
+  return response.status(400).json({
     data: null,
     status: 400,
     errors: array
   })
-}
+};
+
 
 /**
- * @param {import('express').Response} response 
- * @param {any} errors 
+ * Sends a fatal error response with errors.
+ * 
+ * @param {import('express').Response} response The response object.
+ * @param {any} errors The errors to be sent in the response.
  */
 const fatalErrorResponse = (response, errors) => {
-  let array = []
-  if (Array.isArray(errors)) array = errors
-  else array = [errors]
+  let array = [];
+  if (Array.isArray(errors)) array = errors;
+  else array = [errors];
 
   return response.status(500).json({
     data: null,
     status: 500,
     errors: array
-  })
-}
+  });
+};
+
 
 module.exports = {
   okResponse,
+  createdResponse,
   noContentResponse,
   badRequestResponse,
   fatalErrorResponse
