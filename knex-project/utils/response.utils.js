1a0291ef04a38bda8e3635d80e2f05832ba0e51c
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2024-03-16 16:47:17 -0300

    feat: :sparkles: add not found response

--- knex-project/utils/response.utils.js
+++ knex-project/utils/response.utils.js
@@ -1,5 +1,5 @@
 /**
- * Sends a successful response with data.
+ * Sends a successful response with data (HTTP 200).
  * 
  * @param {import('express').Response} response The response object.
  * @param {any} data The data to be sent in the response.
@@ -11,7 +11,7 @@ const okResponse = (response, data) => response.status(200).json({
 });
 
 /**
- * Sends a successful response with data and status 201 (Created).
+ * Sends a successful response with data (HTTP 201).
  * 
  * @param {import('express').Response} response The response object.
  * @param {any} data The data to be sent in the response.
@@ -32,7 +32,7 @@ const noContentResponse = (response) => response.status(204).end();
 
 
 /**
- * Sends a bad request response with errors.
+ * Sends a bad request response with errors (HTTP 400).
  * 
  * @param {import('express').Response} response The response object.
  * @param {any} errors The errors to be sent in the response.
@@ -49,9 +49,27 @@ const badRequestResponse = (response, errors) => {
   })
 };
 
+/**
+ * Sends a response indicating that the requested resource was not found (HTTP 404).
+ * 
+ * @param {import('express').Response} response The response object.
+ * @param {any} errors The errors to be sent in the response.
+ */
+const notFoundResponse = (response, errors) => {
+  // If errors is not an array, convert it to an array
+  let errorArray = Array.isArray(errors) ? errors : [errors];
+
+  // Send a JSON response with a 404 status code and error details
+  return response.status(404).json({
+    data: null,
+    status: 404,
+    errors: errorArray
+  });
+};
+
 
 /**
- * Sends a fatal error response with errors.
+ * Sends a fatal error response with errors (HTTP 500).
  * 
  * @param {import('express').Response} response The response object.
  * @param {any} errors The errors to be sent in the response.
@@ -74,5 +92,6 @@ module.exports = {
   createdResponse,
   noContentResponse,
   badRequestResponse,
+  notFoundResponse,
   fatalErrorResponse
 }
