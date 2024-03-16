596793c258e2d7a10b7f24564cc3e2b95efa9cd8
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2024-03-15 23:16:30 -0300

    refactor: add doc for product creation

--- knex-project/routers/index.router.js
+++ knex-project/routers/index.router.js
@@ -2,6 +2,34 @@ const { createProduct, listAllProducts, deleteProductById } = require('../contro
 
 const rootRouter = require('express').Router()
 
+/**
+ * @openapi
+ * /api/create-product:
+ *   post:
+ *     description: Create a new product.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 required: true
+ *                 description: The name of the product (mandatory).
+ *               description:
+ *                 type: string
+ *                 description: The description of the product.
+ *     responses:
+ *       201:
+ *         description: Product created successfully.
+ *       400:
+ *         description: Bad request.
+ *       500:
+ *         description: Internal server error. Something went wrong.
+ */
+
 rootRouter.post('/create-product', createProduct)
 rootRouter.get('/list-all-products', listAllProducts)
 rootRouter.delete('/delete-product-by-id', deleteProductById)
