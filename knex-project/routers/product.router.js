291fa9333ca5702406a9335e6a7d28db8b6fc714
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2024-03-16 16:35:26 -0300

    docs: :memo: document product module

--- knex-project/routers/product.router.js
+++ knex-project/routers/product.router.js
@@ -8,6 +8,7 @@ const productRouter = Router()
  * @openapi
  * /api/products/create-product:
  *   post:
+ *     summary: Create a new product.
  *     description: Create a new product.
  *     tags:
  *       - Products
@@ -35,7 +36,49 @@ const productRouter = Router()
  *         description: Internal server error. Something went wrong.
  */
 productRouter.post('/create-product', validate(createProductValidation), createProduct)
+/**
+ * @openapi
+ * /api/products/list-all-products:
+ *   get:
+ *     summary: Get a list of all products.
+ *     description: Retrieves a list of all products available in the system.
+ *     tags:
+ *       - Products
+ *     responses:
+ *       200:
+ *         description: A list of products retrieved successfully.
+ *       500:
+ *         description: Internal server error. Something went wrong.
+ */
 productRouter.get('/list-all-products', listAllProducts)
+/**
+ * @openapi
+ * /api/products/delete-product-by-id:
+ *   delete:
+ *     summary: Delete a product by ID.
+ *     description: Deletes a product from the system by its ID.
+ *     tags:
+ *       - Products
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               id:
+ *                 type: integer
+ *                 description: The ID of the product to delete.
+ *             required:
+ *               - id
+ *     responses:
+ *       204:
+ *         description: Product deleted successfully.
+ *       400:
+ *         description: Bad request. The request body is missing or invalid.
+ *       500:
+ *         description: Internal server error. Something went wrong.
+ */
 productRouter.delete('/delete-product-by-id', validate(deleteProductByIdValidation), deleteProductById)
 
 module.exports = productRouter
