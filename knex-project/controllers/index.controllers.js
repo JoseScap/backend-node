c46fee2b33c3106ebe987d4bfdcc783577083d61
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2024-03-15 22:29:02 -0300

    refactor: :zap: improve response handling

--- knex-project/controllers/index.controllers.js
+++ knex-project/controllers/index.controllers.js
@@ -1,5 +1,5 @@
 const db = require("../db")
-const { badRequestResponse, fatalErrorResponse, okResponse } = require("../utils/response.utils")
+const { badRequestResponse, fatalErrorResponse, okResponse, noContentResponse } = require("../utils/response.utils")
 
 /**
  * @param {import('express').Request} req 
@@ -8,16 +8,16 @@ const { badRequestResponse, fatalErrorResponse, okResponse } = require("../utils
 const createProduct = async (req, res) => {
   const { name, description } = req.body
   if (!name) {
-    res.status(400).json(badRequestResponse('Name is mandatory'))
+    badRequestResponse(res, 'Name is mandatory')
     return undefined
   }
 
   try {
     const product = await db.table('products').insert({ name, description })
-    res.json(okResponse(product))
+    okResponse(res, product)
   } catch (error) {
     console.log(error)
-    res.status(500).json(fatalErrorResponse('Something went wrong'))
+    fatalErrorResponse(res, 'Something went wrong')
   }
 }
 
@@ -28,10 +28,10 @@ const createProduct = async (req, res) => {
 const listAllProducts = async (req, res) => {
   try {
     const products = await db.table('products')
-    res.json(okResponse(products))
+    okResponse(res, products)
   } catch (error) {
     console.log(error)
-    res.status(500).json(fatalErrorResponse('Something went wrong'))
+    fatalErrorResponse(res, 'Something went wrong')
   }
 }
 
@@ -42,21 +42,21 @@ const listAllProducts = async (req, res) => {
 const deleteProductById = async (req, res) => {
   const { id } = req.body 
   if (!id) {
-    res.status(400).json(badRequestResponse('Id is mandatory'))
+    badRequestResponse(res, 'Id is mandatory')
     return undefined
   }
 
   if (!Number.isInteger(id)) {
-    res.status(400).json(badRequestResponse('Id must be an integer'))
+    badRequestResponse(res, 'Id must be an integer')
     return undefined
   }
 
   try {
     await db.table('products').where('id', id).del()
-    res.status(204).end()
+    noContentResponse(res)
   } catch (error) {
     console.log(error)
-    res.status(500).json(fatalErrorResponse('Something went wrong'))
+    fatalErrorResponse(res, 'Something went wrong')
   }
 }
 
--- knex-project/utils/response.utils.js
+++ knex-project/utils/response.utils.js
@@ -1,35 +1,53 @@
-const okResponse = (data) => ({
+/**
+ * @param {import('express').Response} response 
+ * @param {any} data 
+ */
+const okResponse = (response, data) => response.status(200).json({
   data,
   status: 200,
   errors: null
 })
 
-const badRequestResponse = (errors) => {
+/**
+ * @param {import('express').Response} response 
+ */
+const noContentResponse = (response) => response.status(204).end()
+
+/**
+ * @param {import('express').Response} response 
+ * @param {any} errors 
+ */
+const badRequestResponse = (response, errors) => {
   let array = []
   if (Array.isArray(errors)) array = errors
   else array = [errors]
 
-  return {
+  return response.status(500).json({
     data: null,
     status: 400,
     errors: array
-  }
+  })
 }
 
-const fatalErrorResponse = (errors) => {
+/**
+ * @param {import('express').Response} response 
+ * @param {any} errors 
+ */
+const fatalErrorResponse = (response, errors) => {
   let array = []
   if (Array.isArray(errors)) array = errors
   else array = [errors]
 
-  return {
+  return response.status(500).json({
     data: null,
     status: 500,
     errors: array
-  }
+  })
 }
 
 module.exports = {
   okResponse,
+  noContentResponse,
   badRequestResponse,
   fatalErrorResponse
 }
