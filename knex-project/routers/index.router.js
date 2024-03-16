c010a9e9fa7464858273bf4445a3ee442d533806
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2024-03-15 23:22:51 -0300

    refactor: :memo: required fields

--- knex-project/routers/index.router.js
+++ knex-project/routers/index.router.js
@@ -7,6 +7,8 @@ const rootRouter = require('express').Router()
  * /api/create-product:
  *   post:
  *     description: Create a new product.
+ *     tags:
+ *       - Products
  *     requestBody:
  *       required: true
  *       content:
@@ -16,11 +18,12 @@ const rootRouter = require('express').Router()
  *             properties:
  *               name:
  *                 type: string
- *                 required: true
- *                 description: The name of the product (mandatory).
+ *                 description: The name of the product.
  *               description:
  *                 type: string
  *                 description: The description of the product.
+ *             required:
+ *               - name
  *     responses:
  *       201:
  *         description: Product created successfully.
@@ -29,7 +32,6 @@ const rootRouter = require('express').Router()
  *       500:
  *         description: Internal server error. Something went wrong.
  */
-
 rootRouter.post('/create-product', createProduct)
 rootRouter.get('/list-all-products', listAllProducts)
 rootRouter.delete('/delete-product-by-id', deleteProductById)
