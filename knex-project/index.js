6c8200fea38e2062e6e09a31c89f64ff08965b42
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2024-03-17 13:51:55 -0300

    style: :lipstick: custom swagger ui

--- knex-project/index.js
+++ knex-project/index.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const morgan = require('morgan')
 const swaggerUi = require('swagger-ui-express')
+const path = require('path')
 
 const rootRouter = require('./routers/index.router')
 const swaggerDocument = require('./openapi/index')
 
 const { PORT } = require('./config')
+const PUBLIC_DIRECTORY = path.join(__dirname, 'public')
 
 const app = express()
+
+app.use(express.static(PUBLIC_DIRECTORY))
 
 app.use(express.json())
 
@@ -15,6 +19,9 @@ app.use(morgan('dev'))
 
 app.use('/api', rootRouter)
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
+  customCss: '.swagger-ui .topbar { background-color: #393939; }',
+  customCssUrl: '/css/swagger-theme.css'
+}))
 
 app.listen(PORT, () => console.log('Server running on port', PORT))
